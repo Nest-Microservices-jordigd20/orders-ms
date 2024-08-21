c51d61740123636f1ad19c1117b980fb86caa353
jordigd20 <jordigd8@gmail.com>
2024-08-21 13:37:19 +0200

    feat: install prisma + postgres with docker-compose

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('OrdersMS-Main');
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port
+    }
+  });
+
+  await app.listen();
 
   logger.log(`OrdersMS is running on port ${envs.port}`);
 }
