c65f9400bda612fc2fc2e37e638e788683c8a83e
jordigd20 <jordigd8@gmail.com>
2024-08-21 18:28:14 +0200

    feat: connect order-ms with products-ms via TCP

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,14 @@ import * as Joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  PRODUCTS_MICROSERVICE_HOST: string;
+  PRODUCTS_MICROSERVICE_PORT: number;
 }
 
 const envSchema = Joi.object({
-  PORT: Joi.number().required()
+  PORT: Joi.number().required(),
+  PRODUCTS_MICROSERVICE_HOST: Joi.string().required(),
+  PRODUCTS_MICROSERVICE_PORT: Joi.number().required()
 }).unknown(true);
 
 const { error, value } = envSchema.validate(process.env);
@@ -18,5 +22,7 @@ if (error) {
 const envVars: EnvVars = value;
 
 export const envs = {
-  port: envVars.PORT
+  port: envVars.PORT,
+  productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
+  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT
 };
